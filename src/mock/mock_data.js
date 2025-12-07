85ac1aa0b18767f2505d2ee6c197ecbe217f7c95
godkissmylife <161803959+godkissmylife@users.noreply.github.com>
2025-12-07 16:20:47 +0800

    chore: 更新导航数据 - 2025/12/7 16:22:27

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -8,14 +8,14 @@ export const mockData = {
       "sites": [
         {
           "id": "cloudflare",
-          "name": "god-cf",
+          "name": "god-cf-Aa2-8",
           "url": "https://dash.cloudflare.com/2468e694678aa35277f33b05246b977f/pages/view/sub-god/settings/production",
           "description": "cf",
           "icon": "/sitelogo/www.cloudflare.com.ico"
         },
         {
           "id": "site-1765024588274",
-          "name": "god-ClouDNS",
+          "name": "god-ClouDNS-Aa2-8",
           "url": "https://www.cloudns.net/main/dashboard/domain-names/",
           "description": "",
           "icon": "https://www.faviconextractor.com/favicon/www.cloudns.net"
